d603c7bcaeea5dbc42a55e86816c2b06cd6b44ee
rookiegao <rookiegao712@gmail>
2022-10-17 11:00:09 +0800

    [docs] add blog file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,10 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath('.'))
+# import os
+# import sys
+#
+# sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 project = 'BigData_Note'
 copyright = '2022, rookiegao'
