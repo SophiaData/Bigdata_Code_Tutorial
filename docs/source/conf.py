012856252453c7b4072a85f037880a0cded900e4
skylines <34996528+rookiegao@users.noreply.github.com>
2022-10-17 09:44:34 +0800

    Update conf.py (#33)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,6 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-apt-get update
 apt-get -y install git rsync python3-pip python3-git python3-stemmer python3-virtualenv python3-setuptools
 python3 -m pip install -U sphinx==4.1.1 myst-parser==0.15.2 pygments==2.10.0 sphinx-rtd-theme==0.5.2
 
