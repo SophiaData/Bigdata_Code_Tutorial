767a731b8339b895f6774b29804cf41ec11b3807
rookiegao <rookiegao712@gmail>
2022-11-15 09:27:55 +0800

    [flink] doris support light schema change

--- Bigdata-flink-example/Flink-demo/src/main/java/io/sophiadata/flink/doris/FlinkToDoris.java
+++ Bigdata-flink-example/Flink-demo/src/main/java/io/sophiadata/flink/doris/FlinkToDoris.java
@@ -2,49 +2,49 @@ package io.sophiadata.flink.doris;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.ververica.cdc.connectors.shaded.org.apache.kafka.connect.json.JsonConverterConfig;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
-import io.sophiadata.flink.base.BaseSql;
+import io.sophiadata.flink.base.BaseCode;
 import org.apache.doris.flink.cfg.DorisExecutionOptions;
 import org.apache.doris.flink.cfg.DorisOptions;
 import org.apache.doris.flink.cfg.DorisReadOptions;
 import org.apache.doris.flink.sink.DorisSink;
-import org.apache.doris.flink.sink.writer.SimpleStringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.UUID;
 
 import static org.apache.flink.shaded.guava30.com.google.common.base.Preconditions.checkNotNull;
 
 /** (@SophiaData) (@date 2022/10/25 10:56). */
-public class FlinkToDoris extends BaseSql {
+public class FlinkToDoris extends BaseCode {
     private static final Logger LOG = LoggerFactory.getLogger(FlinkToDoris.class);
 
     public static void main(String[] args) {
+        // 通过 light schema change 实现 flink ddl 自动同步 Doris
         new FlinkToDoris().init(args, "flink_doris_test", true, true);
         LOG.info(" init 方法正常 ");
     }
 
     @Override
-    public void handle(
-            StreamExecutionEnvironment env, StreamTableEnvironment tEnv, ParameterTool params) {
+    public void handle(StreamExecutionEnvironment env, ParameterTool params) {
         String hostname = checkNotNull(params.get("hostname"));
         int port = checkNotNull(params.getInt("port"));
         String username = checkNotNull(params.get("username"));
         String password = checkNotNull(params.get("password"));
         String databaseList = checkNotNull(params.get("databaseName"));
-        String tableList = checkNotNull(params.get("tableName"));
 
-        // 这是一个未经测试的伪代码
-        Properties properties = new Properties();
-        // decimal 设置为 string 避免转换异常
-        properties.put("decimal.handling.mode", "string");
+        Map<String, Object> customConverterConfigs = new HashMap<>();
+        customConverterConfigs.put(JsonConverterConfig.DECIMAL_FORMAT_CONFIG, "numeric");
+        JsonDebeziumDeserializationSchema schema =
+                new JsonDebeziumDeserializationSchema(false, customConverterConfigs);
 
         MySqlSource<String> sourceFunction =
                 MySqlSource.<String>builder()
@@ -53,35 +53,44 @@ public class FlinkToDoris extends BaseSql {
                         .username(username)
                         .password(password)
                         .databaseList(databaseList)
-                        .tableList(tableList)
-                        .deserializer(new JsonDebeziumDeserializationSchema())
+                        .tableList("test2")
+                        .deserializer(schema)
                         .includeSchemaChanges(true) // 接收 ddl
                         .startupOptions(StartupOptions.initial())
-                        .debeziumProperties(properties)
+                        .serverTimeZone("Asia/Shanghai")
+                        .debeziumProperties(DateToStringConverter.DEFAULT_PROPS)
                         .build();
 
-        DataStreamSource<String> mysql =
-                env.fromSource(sourceFunction, WatermarkStrategy.noWatermarks(), "mysql")
-                        .setParallelism(1);
-
-        mysql.print().setParallelism(1);
+        Properties props = new Properties();
+        props.setProperty("format", "json");
+        props.setProperty("read_json_by_line", "true");
+        DorisOptions dorisOptions =
+                DorisOptions.builder()
+                        .setFenodes("127.0.0.1:8030")
+                        .setTableIdentifier("test.t1")
+                        .setUsername("root")
+                        .setPassword("")
+                        .build();
+        //
+        DorisExecutionOptions.Builder executionBuilder = DorisExecutionOptions.builder();
+        executionBuilder
+                .setLabelPrefix("label-doris" + UUID.randomUUID())
+                .setStreamLoadProp(props)
+                .setDeletable(true);
 
         DorisSink.Builder<String> builder = DorisSink.builder();
-        DorisOptions.Builder dorisBuilder = DorisOptions.builder();
-        dorisBuilder
-                .setFenodes("***")
-                .setTableIdentifier("***")
-                .setUsername("***")
-                .setPassword("***");
-
-        DorisExecutionOptions.Builder executionBuilder = DorisExecutionOptions.builder();
-        executionBuilder.setLabelPrefix("label-doris");
-
         builder.setDorisReadOptions(DorisReadOptions.builder().build())
                 .setDorisExecutionOptions(executionBuilder.build())
-                .setSerializer(new SimpleStringSerializer()) // serialize according to string
-                .setDorisOptions(dorisBuilder.build());
+                .setDorisOptions(dorisOptions)
+                .setSerializer(
+                        JsonDebeziumSchemaSerializer.builder()
+                                .setDorisOptions(dorisOptions)
+                                .build());
 
-        mysql.sinkTo(builder.build());
+        env.fromSource(
+                        sourceFunction,
+                        WatermarkStrategy.noWatermarks(),
+                        "MySQL Source") // .print();
+                .sinkTo(builder.build());
     }
 }
---
+++ Bigdata-flink-example/Flink-demo/src/main/java/io/sophiadata/flink/doris/JsonDebeziumSchemaSerializer.java
@@ -0,0 +1,251 @@
+package io.sophiadata.flink.doris;
+
+import org.apache.flink.annotation.VisibleForTesting;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.doris.flink.cfg.DorisOptions;
+import org.apache.doris.flink.sink.writer.DorisRecordSerializer;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.apache.doris.flink.sink.writer.LoadConstants.DORIS_DELETE_SIGN;
+
+/** (@SophiaData) (@date 2022/11/15 09:02). */
+public class JsonDebeziumSchemaSerializer implements DorisRecordSerializer<String> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(JsonDebeziumSchemaSerializer.class);
+    private static final String CHECK_SCHEMA_CHANGE_API =
+            "http://%s/api/enable_light_schema_change/%s/%s";
+    private static final String SCHEMA_CHANGE_API = "http://%s/api/query/default_cluster/%s";
+    private static final String OP_READ = "r"; // snapshot read
+    private static final String OP_CREATE = "c"; // insert
+    private static final String OP_UPDATE = "u"; // update
+    private static final String OP_DELETE = "d"; // delete
+
+    private static final String addDropDDLRegex =
+            "ALTER\\s+TABLE\\s+[^\\s]+\\s+(ADD|DROP)\\s+COLUMN\\s+([^\\s]+).*";
+    private final Pattern addDropDDLPattern;
+    private final DorisOptions dorisOptions;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public JsonDebeziumSchemaSerializer(DorisOptions dorisOptions, Pattern pattern) {
+        this.dorisOptions = dorisOptions;
+        this.addDropDDLPattern =
+                pattern == null
+                        ? Pattern.compile(addDropDDLRegex, Pattern.CASE_INSENSITIVE)
+                        : pattern;
+    }
+
+    @Override
+    public byte[] serialize(String record) throws IOException {
+        LOG.debug("received debezium json data {} :", record);
+        JsonNode recordRoot = objectMapper.readTree(record);
+        String op = extractJsonNode(recordRoot, "op");
+        if (Objects.isNull(op)) {
+            // schema change ddl
+            schemaChange(recordRoot);
+            return null;
+        }
+        Map<String, String> valueMap;
+        switch (op) {
+            case OP_READ:
+            case OP_CREATE:
+            case OP_UPDATE:
+                valueMap = extractAfterRow(recordRoot);
+                addDeleteSign(valueMap, false);
+                break;
+            case OP_DELETE:
+                valueMap = extractBeforeRow(recordRoot);
+                addDeleteSign(valueMap, true);
+                break;
+            default:
+                LOG.error("parse record fail, unknown op {} in {}", op, record);
+                return null;
+        }
+        return objectMapper.writeValueAsString(valueMap).getBytes(StandardCharsets.UTF_8);
+    }
+
+    @VisibleForTesting
+    public boolean schemaChange(JsonNode recordRoot) {
+        boolean status = false;
+        try {
+            boolean doSchemaChange = checkSchemaChange(recordRoot);
+            status = doSchemaChange && execSchemaChange(recordRoot);
+            LOG.info("schema change status:{}", status);
+        } catch (Exception ex) {
+            LOG.warn("schema change error :", ex);
+        }
+        return status;
+    }
+
+    private void addDeleteSign(Map<String, String> valueMap, boolean delete) {
+        if (delete) {
+            valueMap.put(DORIS_DELETE_SIGN, "1");
+        } else {
+            valueMap.put(DORIS_DELETE_SIGN, "0");
+        }
+    }
+
+    private boolean checkSchemaChange(JsonNode record) throws IOException {
+        String database = extractDatabase(record);
+        String table = extractTable(record);
+        String requestUrl =
+                String.format(CHECK_SCHEMA_CHANGE_API, dorisOptions.getFenodes(), database, table);
+        Map<String, Object> param = buildRequestParam(record);
+        if (param.size() != 2) {
+            return false;
+        }
+        HttpGetWithEntity httpGet = new HttpGetWithEntity(requestUrl);
+        httpGet.setHeader(HttpHeaders.AUTHORIZATION, authHeader());
+        httpGet.setEntity(new StringEntity(objectMapper.writeValueAsString(param)));
+        boolean success = handleResponse(httpGet);
+        if (!success) {
+            LOG.warn("schema change can not do table {}.{}", database, table);
+        }
+        return success;
+    }
+
+    /** Build param { "isDropColumn": true, "columnName" : "column" } */
+    private Map<String, Object> buildRequestParam(JsonNode record) throws IOException {
+        Map<String, Object> params = new HashMap<>();
+        String ddl = extractDDL(record);
+        if (ddl == null) {
+            return params;
+        }
+        Matcher matcher = addDropDDLPattern.matcher(ddl);
+        if (matcher.find()) {
+            String op = matcher.group(1);
+            String col = matcher.group(2);
+            params.put("isDropColumn", op.equalsIgnoreCase("DROP"));
+            params.put("columnName", col);
+        }
+        return params;
+    }
+
+    private boolean execSchemaChange(JsonNode record) throws IOException {
+        String extractDDL = extractDDL(record);
+        Map<String, String> param = new HashMap<>();
+        param.put("stmt", extractDDL);
+        String database = extractDatabase(record);
+        String requestUrl = String.format(SCHEMA_CHANGE_API, dorisOptions.getFenodes(), database);
+        HttpPost httpPost = new HttpPost(requestUrl);
+        httpPost.setHeader(HttpHeaders.AUTHORIZATION, authHeader());
+        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+        httpPost.setEntity(new StringEntity(objectMapper.writeValueAsString(param)));
+        return handleResponse(httpPost);
+    }
+
+    private String extractDatabase(JsonNode record) {
+        return extractJsonNode(record.get("source"), "db");
+    }
+
+    private String extractTable(JsonNode record) {
+        return extractJsonNode(record.get("source"), "table");
+    }
+
+    private boolean handleResponse(HttpUriRequest request) {
+        try (CloseableHttpClient httpclient = HttpClients.createDefault()) {
+            CloseableHttpResponse response = httpclient.execute(request);
+            final int statusCode = response.getStatusLine().getStatusCode();
+            if (statusCode == 200 && response.getEntity() != null) {
+                String loadResult = EntityUtils.toString(response.getEntity());
+                Map<String, Object> responseMap = objectMapper.readValue(loadResult, Map.class);
+                String code = responseMap.getOrDefault("code", "-1").toString();
+                if (code.equals("0")) {
+                    return true;
+                } else {
+                    LOG.error("schema change response:{}", loadResult);
+                }
+            }
+        } catch (Exception e) {
+            LOG.error("http request error,", e);
+        }
+        return false;
+    }
+
+    private String extractJsonNode(JsonNode record, String key) {
+        return record != null && record.get(key) != null ? record.get(key).asText() : null;
+    }
+
+    private Map<String, String> extractBeforeRow(JsonNode record) {
+        return extractRow(record.get("before"));
+    }
+
+    private Map<String, String> extractAfterRow(JsonNode record) {
+        return extractRow(record.get("after"));
+    }
+
+    private Map<String, String> extractRow(JsonNode recordRow) {
+        Map<String, String> recordMap =
+                objectMapper.convertValue(recordRow, new TypeReference<Map<String, String>>() {});
+        return recordMap != null ? recordMap : new HashMap<>();
+    }
+
+    @VisibleForTesting
+    public String extractDDL(JsonNode record) throws IOException {
+        String historyRecord = extractJsonNode(record, "historyRecord");
+        if (Objects.isNull(historyRecord)) {
+            return null;
+        }
+        String ddl = extractJsonNode(objectMapper.readTree(historyRecord), "ddl");
+        if (!Objects.isNull(ddl)) {
+            // filter add/drop operation
+            if (addDropDDLPattern.matcher(ddl).matches()) {
+                return ddl;
+            }
+        }
+        LOG.info("parse ddl:{}", ddl);
+        return null;
+    }
+
+    private String authHeader() {
+        return "Basic "
+                + new String(
+                        Base64.encodeBase64(
+                                (dorisOptions.getUsername() + ":" + dorisOptions.getPassword())
+                                        .getBytes(StandardCharsets.UTF_8)));
+    }
+
+    public static JsonDebeziumSchemaSerializer.Builder builder() {
+        return new JsonDebeziumSchemaSerializer.Builder();
+    }
+
+    /** Builder for JsonDebeziumSchemaSerializer. */
+    public static class Builder {
+        private DorisOptions dorisOptions;
+        private Pattern addDropDDLPattern;
+
+        public JsonDebeziumSchemaSerializer.Builder setDorisOptions(DorisOptions dorisOptions) {
+            this.dorisOptions = dorisOptions;
+            return this;
+        }
+
+        public JsonDebeziumSchemaSerializer.Builder setPattern(Pattern addDropDDLPattern) {
+            this.addDropDDLPattern = addDropDDLPattern;
+            return this;
+        }
+
+        public JsonDebeziumSchemaSerializer build() {
+            return new JsonDebeziumSchemaSerializer(dorisOptions, addDropDDLPattern);
+        }
+    }
+}
