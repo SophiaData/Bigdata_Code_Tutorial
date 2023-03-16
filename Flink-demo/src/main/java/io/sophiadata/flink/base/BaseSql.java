00754e0b81e26bb7210755cc5b68ff2b90da5f14
skylines <34996528+gaotingkai@users.noreply.github.com>
2023-03-16 14:29:51 +0800

    [flink] Optimize flink task failure processing logic (#134)
    Co-authored-by: gaotingkai <rookiegao712@gmail>

--- Flink-demo/src/main/java/io/sophiadata/flink/base/BaseSql.java
+++ Flink-demo/src/main/java/io/sophiadata/flink/base/BaseSql.java
@@ -95,8 +95,6 @@ public abstract class BaseSql {
     }
 
     public void restartTask(StreamExecutionEnvironment env) {
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, Time.seconds(10)));
-        env.setRestartStrategy(
-                RestartStrategies.failureRateRestart(10, Time.minutes(5), Time.seconds(10)));
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(10, Time.seconds(10)));
     }
 }
