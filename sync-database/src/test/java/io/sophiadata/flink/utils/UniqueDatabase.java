518e6f60c900ac553752bd52c870b1bb5f3d298d
skylines <34996528+gaotingkai@users.noreply.github.com>
2023-06-01 14:53:29 +0800

    [flink] test code (#150)
    Co-authored-by: gaotingkai <rookiegao712@gmail>

--- sync-database/src/test/java/io/sophiadata/flink/utils/UniqueDatabase.java
+++ sync-database/src/test/java/io/sophiadata/flink/utils/UniqueDatabase.java
@@ -1,10 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -15,7 +16,7 @@
  * limitations under the License.
  */
 
-package io.sophiadata.flink.utis;
+package io.sophiadata.flink.utils;
 
 import java.net.URL;
 import java.nio.file.Files;
