d57d3a589374b1753f137641f3081cb23b54dfa5
xingjieshitu <104996156+xingjieshitu@users.noreply.github.com>
2023-06-01 15:30:21 +0800

    [fix] oracle sink stmt failed to close normally (#144)
    Co-authored-by: skylines <34996528+gaotingkai@users.noreply.github.com>

--- Flink-demo/src/main/java/io/sophiadata/flink/sync2/executor/KafkaToOracle4.java
+++ Flink-demo/src/main/java/io/sophiadata/flink/sync2/executor/KafkaToOracle4.java
@@ -45,6 +45,7 @@ import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -349,6 +350,14 @@ public class KafkaToOracle4 {
 
                         } catch (Exception e) {
                             LOG.error("  异常： " + e + " - " + s1);
+                        } finally {
+                            if (stmt != null) {
+                                try {
+                                    stmt.close();
+                                } catch (SQLException e) {
+                                    LOG.error(" stmt Exception: {}" + e.getMessage());
+                                }
+                            }
                         }
                     }
                 });
